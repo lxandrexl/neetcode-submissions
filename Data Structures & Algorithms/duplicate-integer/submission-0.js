7e84afc5e70b3dd0da08d4a6b3d381a0bd6be7ac
Joshua <jovillenad@gmail.com>
2026-06-21 18:56:36 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    hasDuplicate(nums) {
+        const set = new Set();
+        for (const num of nums) {
+            if(set.has(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
